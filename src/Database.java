5409b8ef2ad2286b307162f6b35dddc2d2ffd489
Lenovo <Lenovo@Lenovo-PC>
2016-12-19 15:46:17 +0800

    Server:write database Client:create the frame

--- src/Database.java
+++ src/Database.java
@@ -15,6 +15,362 @@ public class Database {
 	
 	public Database() 
 	{
+		try 
+		{
+			Class.forName("com.mysql.jdbc.Driver");                         		//加载驱动程序
+			System.out.println("load driver success");
+			
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/myonlinedic","root","123456");
+			System.out.println("connection success");
+			
+			statement = connection.createStatement();
+			
+		} 
+		catch (ClassNotFoundException e) 
+		{
+			e.printStackTrace();
+		}                                   
 		
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
 	}
+	
+	public int findPort(String curName)
+	{
+		int port = -1;                        //若用户不在线，则返回-1
+		if(curName == null)
+			return -1;
+		try
+		{
+			preparedStatement = connection.prepareStatement("select port from user where name = ? and isLogin = 1");
+			preparedStatement.setString(1,curName);
+			
+			resultSet = preparedStatement.executeQuery();
+			
+			if(resultSet.next())
+				port = resultSet.getInt(1);                                 
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		return port;
+	}
+	
+	public int isLoginSuccess(String curName,String curPassword)                                  //判断是否登录成功
+	{
+		int loginSuccess = 0; //代表登录失败
+		try
+		{
+			preparedStatement = connection.prepareStatement("select * from user where name = ? and password = ?");
+			preparedStatement.setString(1,curName);
+			preparedStatement.setString(2,curPassword);
+			resultSet = preparedStatement.executeQuery();
+			if(resultSet.next())
+			{
+				if(resultSet.getInt(5) == 0)
+					loginSuccess = 1; //成功登录
+				else
+					loginSuccess = 2;//已被登录
+			}
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		return loginSuccess;
+	}
+	
+	public boolean isRegisterSuccess(String curName)
+	{
+		boolean isSuccess = true;
+		try
+		{
+			preparedStatement = connection.prepareStatement("select * from user where name = ? ");
+			preparedStatement.setString(1,curName);
+			resultSet = preparedStatement.executeQuery();
+			
+			if(resultSet.next())
+				isSuccess = false;
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		return isSuccess;
+	}
+	
+	
+	public void insertRegister(String curName,String curPassword,String curIP_address,int curPort)       //注册时向数据库插入一条信息
+	{
+		try
+		{
+			preparedStatement = connection.prepareStatement("insert into user values(?,?,?,?,?)");
+			preparedStatement.setString(1,curName);
+			preparedStatement.setString(2,curPassword);
+			preparedStatement.setString(3,curIP_address);
+			preparedStatement.setInt(4,curPort);
+			preparedStatement.setInt(5,1);                                                              //是登录状态
+			preparedStatement.executeUpdate();
+		}
+		catch (SQLException e)
+		{                  
+			e.printStackTrace();
+		}
+	}
+	
+	public void changeToLoginState(String curName)
+	{
+		try
+		{
+			preparedStatement = connection.prepareStatement("update user set isLogin = 1 where name = ? ");
+			preparedStatement.setString(1,curName);
+			preparedStatement.executeUpdate();
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void changeToExitState(String curName)
+	{
+		try
+		{
+			preparedStatement = connection.prepareStatement("update user set isLogin = 0 where name = ? ");
+			preparedStatement.setString(1,curName);
+			preparedStatement.executeUpdate();
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void changePort(int curPort,String curName)
+	{
+		try
+		{
+			preparedStatement = connection.prepareStatement("update user set port = ? where name = ? ");
+			preparedStatement.setInt(1,curPort);
+			preparedStatement.setString(2,curName);
+			preparedStatement.executeUpdate();
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public int countOnlineUser()
+	{
+		int number = 0;
+		try
+		{
+			statement =connection.createStatement();
+			resultSet = statement.executeQuery("select count(*) from user where isLogin = 1");
+			if(resultSet.next())
+				number = resultSet.getInt(1);
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		return number;
+	}
+	
+	public int[] getPorts()
+	{
+		int length = countOnlineUser();
+		
+		int[] loginPort = new int[length];
+		try
+		{			
+			statement =connection.createStatement();
+			resultSet = statement.executeQuery("select port from user where isLogin = 1");
+			int index =0;
+			while(resultSet.next())
+			{
+				loginPort[index] = resultSet.getInt(1);
+				index++;
+			}
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		return loginPort;
+	}
+	
+	public int getPort(String curname)
+	{
+		int port = 0;
+		try
+		{	
+			preparedStatement = connection.prepareStatement("select port from user where name = ?");
+			preparedStatement.setString(1,curname);
+			resultSet = preparedStatement.executeQuery();
+			
+			while(resultSet.next())
+			{
+				port = resultSet.getInt(1);
+			}
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		return port;
+	}
+	
+	public String[] getUserList()
+	{
+		String[] userList = null;
+		try
+		{
+			statement =connection.createStatement();
+			resultSet = statement.executeQuery("select name from user where isLogin = 1");
+			int length = 0;
+			while(resultSet.next())
+				length++;
+			if(length == 0)
+				return null;
+			
+			userList = new String[length];
+			for(int i = 0;i < length;i++)
+				userList[i] = "";
+			
+			
+			resultSet.first();
+			for(int index = 0;index < length;index++)
+			{
+				userList[index] = resultSet.getString(1);
+				resultSet.next();
+			}
+			
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return userList;
+	 }
+	
+	/*翻译和点赞*/
+	public int[] getThreePraise(String curWord)                                	   //返回指定单词在百度、有道、必应的点赞次数
+	{
+		int[] resultArr = new int[3];
+		try 
+		{
+			preparedStatement = connection.prepareStatement("select baidu_praise,youdao_praise,biying_praise from words where word = ?");
+			preparedStatement.setString(1,curWord);
+			resultSet = preparedStatement.executeQuery();
+			while(resultSet.next())
+			{
+			    resultArr[0] = resultSet.getInt(1);
+			    resultArr[1] = resultSet.getInt(2);
+			    resultArr[2] = resultSet.getInt(3);
+			}	
+			
+		}
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		return resultArr;
+	}
+	
+	public void updateBaiduPraise(String curWord)
+	{
+		try
+		{
+			preparedStatement = connection.prepareStatement("update words set baidu_praise = baidu_praise + 1 where word = ?");
+			preparedStatement.setString(1,curWord);
+			
+			preparedStatement.executeUpdate();
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void updateYoudaoPraise(String curWord)
+	{
+		try
+		{
+			preparedStatement = connection.prepareStatement("update words set youdao_praise = youdao_praise + 1 where word = ?");
+			preparedStatement.setString(1,curWord);
+			
+			preparedStatement.executeUpdate();
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void updateBiyingPraise(String curWord)
+	{
+		try
+		{
+			preparedStatement = connection.prepareStatement("update words set biying_praise = biying_praise + 1 where word = ?");
+			preparedStatement.setString(1,curWord);
+			
+			preparedStatement.executeUpdate();
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public String[] getThreeTranslation(String curWord)
+	{
+		String[] threeTranslation = new String[3];
+		for(int i = 0;i < 3;i++)
+			threeTranslation[i] = "";
+		try 
+		{
+			preparedStatement = connection.prepareStatement("select baidu_exp,youdao_exp,biying_exp from words where word = ?");
+			preparedStatement.setString(1,curWord);
+			resultSet = preparedStatement.executeQuery();
+			
+			if(resultSet.next())
+			{
+				threeTranslation[0] = resultSet.getString(1);
+				threeTranslation[1] = resultSet.getString(2);
+				threeTranslation[2] = resultSet.getString(3);
+			}
+			else                                          					//未在当地数据库中找到，爬网！更新数据库
+			{
+				threeTranslation[0] = explanation.Baidu_exp(curWord);
+				threeTranslation[1] = explanation.Youdao_exp(curWord);
+				threeTranslation[2] = explanation.Biying_exp(curWord);
+				
+				if(threeTranslation[0] != null)
+				{
+					preparedStatement = connection.prepareStatement("insert into words values(?,?,?,?,?,?,?)");
+					preparedStatement.setString(1,curWord);
+					preparedStatement.setString(2,threeTranslation[0]);
+					preparedStatement.setInt(3,0);
+					preparedStatement.setString(4,threeTranslation[1]);
+					preparedStatement.setInt(5,0);                                                              
+					preparedStatement.setString(6,threeTranslation[2]);
+					preparedStatement.setInt(7,0);    
+					preparedStatement.executeUpdate();
+				}
+			}
+			
+		}
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		return threeTranslation;
+	}
+	
 }
--- src/ShowDictionary.java
+++ src/ShowDictionary.java
@@ -12,6 +12,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Vector;
+
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
@@ -19,6 +20,165 @@ import javax.swing.event.ListSelectionListener;
 
 public class ShowDictionary extends JFrame{
 
+	private JButton register = new JButton();
+	private JButton login = new JButton();
+	private JTextField input = new JTextField();
+	private JButton search = new JButton();
+	private JCheckBox baiduChoose = new JCheckBox();
+	private JCheckBox youdaoChoose = new JCheckBox();
+	private JCheckBox biyingChoose = new JCheckBox();
+	private JList<String> userOnline = new JList<String>();
+	private JTextArea baiduInterpret = new JTextArea();
+	private JTextArea youdaoInterpret = new JTextArea();
+	private JTextArea biyingInterpret = new JTextArea();
+	private JTextField baiduUserId = new JTextField();
+	private JTextField youdaoUserId = new JTextField();
+	private JTextField biyingUserId = new JTextField();
+	private JButton baiduSendCard = new JButton();
+	private JButton youdaoSendCard = new JButton();
+	private JButton biyingSendCard = new JButton();
+	private JButton baiduPraise = new JButton();
+	private JButton youdaoPraise = new JButton();
+	private JButton biyingPraise = new JButton();
+	private JButton baiduSendAll = new JButton();//群发按钮
+	private JButton youdaoSendAll = new JButton();
+	private JButton biyingSendAll = new JButton();
+	private JPanel registerAndLogin = new JPanel();
+	private JPanel baidu = new JPanel();
+	private JPanel youdao = new JPanel();
+	private JPanel biying = new JPanel();
+	private JPanel interpret = new JPanel();//显示三个网站的翻译,设置为属性，以便后续根据点赞数量调整三个翻译的顺序
+	private JPanel panel = new JPanel();//将所有组建放在panel上
+	private Vector<String> vector = new Vector<String>();
+	private JPanel panelUp = new JPanel();//显示登录、注册、搜索、三个选项
+	private JPanel panelOfChoose = new JPanel();//显示三个选项
+	private JPanel baiduSendWord = new JPanel();//显示百度用户id框和发送按钮和点赞按钮
+	private JPanel youdaoSendWord = new JPanel();//显示有道用户id框和发送按钮和点赞按钮
+	private JPanel biyingSendWord = new JPanel();//显示必应用户id框和发送按钮和点赞按钮
+	private JPanel userpanel = new JPanel();//显示在线用户列表
+	private JTextField userText = new JTextField();//用户名
+	private JButton logoff = new JButton();//退出登录按钮
+	private static DataOutputStream toServer;
+	private static DataInputStream fromServer;
+	
+	JLabel lab;
+	boolean skinIsPicture = false;
+	
+	//数据
+	private String[] threeTranslation;
+	private int[] threePraise;
+	
+	private String sendUserName;
+	private String sendTranslation;
+	
+	private String userName;                      //当前登录用户的名称
+	
+	public ShowDictionary()
+	{
+		threeTranslation = new String[3];
+		baiduUserId.setEditable(false);
+		youdaoUserId.setEditable(false);
+		biyingUserId.setEditable(false);
+		for(int i = 0;i < 3;i++)
+			threeTranslation[i] = "";
+		threePraise = new int[3];
+		
+		panel.setLayout(new BorderLayout(10,10));
+		
+		panelUp.setLayout(new BorderLayout(10,10));
+		registerAndLogin.setLayout(new FlowLayout(FlowLayout.RIGHT));							//显示登陆、换肤和注册按钮
+		register.setText("注册");
+		login.setText("登录");
+		logoff.setText("退出");
+		userText.setEditable(false);
+		userText.setText("游客");
+		
+		registerAndLogin.add(login);
+		registerAndLogin.add(register);
+		registerAndLogin.add(logoff);
+		registerAndLogin.add(userText);
+		logoff.setVisible(false);
+		panelOfChoose.setLayout(new FlowLayout());
+		baiduChoose.setText("百度");
+		youdaoChoose.setText("有道");
+		biyingChoose.setText("必应");
+		panelOfChoose.add(baiduChoose);
+		panelOfChoose.add(youdaoChoose);
+		panelOfChoose.add(biyingChoose);
+		panelUp.add(registerAndLogin,BorderLayout.NORTH);
+		panelUp.add(new JLabel(" Input"), BorderLayout.WEST);
+		panelUp.add(input, BorderLayout.CENTER);
+		search.setText("search");
+		panelUp.add(search, BorderLayout.EAST);
+		panelUp.add(panelOfChoose,BorderLayout.SOUTH);
+		
+		
+		interpret.setLayout(new GridLayout(3,1));
+		baiduSendWord.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		baiduSendCard.setText("发送单词卡");
+		baiduPraise.setText("赞 ");
+		baiduSendWord.add(new JLabel("用户ID："));
+		baiduSendAll.setText("群发");
+		baiduSendWord.add(baiduUserId);
+		baiduSendWord.add(baiduSendCard);
+		baiduSendWord.add(baiduSendAll);
+		baiduSendWord.add(baiduPraise);
+		baidu.setLayout(new BorderLayout(10,10));
+		baidu.add(new JLabel("百度"),BorderLayout.WEST);
+		baiduInterpret.setEditable(false);
+		baiduUserId.setColumns(15);
+		baidu.add(new JScrollPane(baiduInterpret),BorderLayout.CENTER);
+		baidu.add(baiduSendWord,BorderLayout.SOUTH);
+		youdaoSendWord.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		youdaoSendCard.setText("发送单词卡");
+		youdaoPraise.setText("赞");
+		youdaoSendWord.add(new JLabel("用户ID："));
+		youdaoSendAll.setText("群发");
+		youdaoSendWord.add(youdaoUserId);
+		youdaoSendWord.add(youdaoSendCard);
+		youdaoSendWord.add(youdaoSendAll);
+		youdaoSendWord.add(youdaoPraise);
+		youdao.setLayout(new BorderLayout(10,10));
+		youdao.add(new JLabel("有道"),BorderLayout.WEST);
+		youdaoInterpret.setEditable(false);
+		youdaoUserId.setColumns(15);
+		youdao.add(new JScrollPane(youdaoInterpret),BorderLayout.CENTER);
+		youdao.add(youdaoSendWord,BorderLayout.SOUTH);
+		biyingSendWord.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		biyingSendCard.setText("发送单词卡");
+		biyingPraise.setText("赞");
+		biyingSendWord.add(new JLabel("用户ID："));
+		biyingSendAll.setText("群发");
+		biyingSendWord.add(biyingUserId);
+		biyingSendWord.add(biyingSendCard);
+		biyingSendWord.add(biyingSendAll);
+		biyingSendWord.add(biyingPraise);
+		biying.setLayout(new BorderLayout(10,10));
+		biying.add(new JLabel("必应"),BorderLayout.WEST);
+		biyingInterpret.setEditable(false);
+		biyingUserId.setColumns(15);
+		biying.add(new JScrollPane(biyingInterpret),BorderLayout.CENTER);
+		biying.add(biyingSendWord,BorderLayout.SOUTH);
+		interpret.add(baidu);
+		interpret.add(youdao);
+		interpret.add(biying);
+		
+		panel.add(panelUp,BorderLayout.NORTH);
+		userpanel.setLayout(new BorderLayout(5,5));
+		userpanel.add(new JLabel("在线用户列表："),BorderLayout.NORTH);
+		userpanel.add(userOnline,BorderLayout.CENTER);
+		panel.add(userpanel,BorderLayout.WEST);
+		panel.add(interpret,BorderLayout.CENTER);
+		add(panel);
+		
+		userpanel.setVisible(false);
+		baidu.setVisible(false);
+		youdao.setVisible(false);
+		biying.setVisible(false);
+		
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
